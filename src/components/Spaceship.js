3b9af9439f77f7c2a8c7c83ae212cb41b3cf8317
Felice Forby <feliceforby@gmail.com>
2019-03-16 02:09:42 +0000

    Done.

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 class Spaceship extends React.Component {
   render() {
+    const { name, speed, hasRockets, colors } = this.props;
+
     return(
       <div className="spaceship">
-        <h2>{this.props.name}</h2>
-        <p>Speed: {this.props.speed}</p>
-        <p>Rockets: {this.props.hasRockets}</p>
+        <h2>{name}</h2>
+        <p>Speed: {speed}</p>
+        <p>Rockets: {hasRockets ? "Equipped" : "Not Equipped"}</p>
         <p>Colors: {this.props.colors.join(', ')}</p>
       </div>
     )
